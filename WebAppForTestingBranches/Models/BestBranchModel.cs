9e280d95af07c7d2b30ddf2697d1a055621db1e7
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2019-05-03 16:39:40 +0300

    Initial branch commit

---
+++ WebAppForTestingBranches/Models/BestBranchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppForTestingBranches.Models
+{
+    public class BestBranchModel
+    {
+        public int Id { get; set; }
+    }
+}
